a7e2e322dd4c291c52cce8eba8a08e842bec9818
bhushang098 <bhushangurnule1999@gmail.com>
2022-05-31 11:20:44 +0530

    module 5.3

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,7 @@ class Quiz extends Component{
 
     render()
     {
-        const isQuizEnd = ((this.state.quiz_position[-1]) === 
+        const isQuizEnd = ((this.state.quiz_position - 1) === 
         quizData.quiz_questions.length)
       return <div>
                 {
