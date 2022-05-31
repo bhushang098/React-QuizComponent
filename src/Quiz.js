7d9404e3557effee014c9fe83c360b82d43acd1b
bhushang098 <bhushangurnule1999@gmail.com>
2022-05-31 10:52:36 +0530

    module 3.1

--- src/Quiz.js
+++ src/Quiz.js
@@ -1,5 +1,7 @@
 import React, {Component} from "react"
 
+import QuizQuestion from "./QuizQuestion"
+
 let quizData = require("./quiz_data.json")
 
 class Quiz extends Component{
@@ -13,7 +15,7 @@ class Quiz extends Component{
     render()
     {
       return <div>
-                <div className="QuizQuestion">{quizData.quiz_questions[0].instruction_text}</div>
+                <QuizQuestion className="QuizQuestion" quiz_question ={quizData.quiz_questions[1]}/>
             </div>
     }
 }
